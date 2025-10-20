1caa05dba02b4f45b55ce7236ea39db03b8db40b
raman24dhaliwal <raman24dhaliwal@gmail.com>
2025-10-20 16:38:32 -0700

    Update message.py

---
+++ backend/models/message.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+from database import db
+
+class Message(db.Model):
+    __tablename__ = "messages"
+    id = db.Column(db.Integer, primary_key=True)
+    sender_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    receiver_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=True)
+    content = db.Column(db.String(2000))
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "sender_id": self.sender_id,
+            "receiver_id": self.receiver_id,
+            "content": self.content,
+            "timestamp": self.timestamp.isoformat(),
+        }
